f0e9a970c3ef6404c5fc6d080ecc64eecd6d768b
davidcoats <david@getsafetycone.com>
2023-12-14 01:34:05 -0800

    Interim changes.

---
+++ source/R5T.L0057/Code/Functionality/IXElementOperator.cs
@@ -0,0 +1,14 @@
+using System;
+
+using R5T.T0132;
+
+
+namespace R5T.L0057
+{
+    [FunctionalityMarker]
+    public partial interface IXElementOperator : IFunctionalityMarker,
+        L0056.IXElementOperator
+    {
+
+    }
+}
